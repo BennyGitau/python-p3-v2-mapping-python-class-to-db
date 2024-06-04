6f6f8a65e9a7a95708f481896c69f35509d69295
BensonGitau <bensonkamaugitau@gmail.com>
2024-06-04 18:38:00 +0300

    ORM

--- lib/debug.py
+++ lib/debug.py
@@ -5,5 +5,28 @@ from __init__ import CONN, CURSOR
 from department import Department
 
 import ipdb
+Department.drop_table()
+Department.create_table()
+#creating instances
+#payroll = Department("Payroll", "Building A, 5th Floor")
+#print(payroll) #print before save will return the row with id as None
+
+#payroll.save()
+#print(payroll)# print after save will assign object id attribute
+ 
+#use create class method to combine instance and saving
+payroll = Department.create("Payroll", "Building A, 5th Floor")
+
+hr = Department.create("Human Resources", "Building C, East Wing")
+
+#updating a table row
+hr.name = 'HR'
+hr.location = "Building F, 10th floor"
+hr.update()
+
+#deleting a table row
+payroll.delete()
+
+
 
 ipdb.set_trace()
